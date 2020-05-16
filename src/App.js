4a4189b5222c851c035d9305ff993398bffd728a
Robert Laws <roblaws@me.com>
2020-05-16 15:26:14 +0300

    update files to prepare for code along

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Button } from "@material-ui/core";
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button variant="contained">
-          Click Me!
-        </Button>
-      </header>
+      Hello!
     </div>
   );
 }
