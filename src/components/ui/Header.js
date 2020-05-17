dcd291ab86b6fc0292d1c4be63b6c59f9489955c
Robert Laws <roblaws@me.com>
2020-05-17 15:26:21 +0300

    setup navigation and routes

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { AppBar, Toolbar, useScrollTrigger, Typography, Tabs, Tab, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/blob-logo.svg';
 
@@ -23,8 +24,18 @@ const useStyles = makeStyles(theme => ({
     marginBottom: '0.75em'
   },
   logo: {
-    height: '4em',
+    height: '5em',
     margin: '5px 10px'
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
+  },
+  logoLink: {
+    color: '#FFFFFF',
+    textDecoration: 'none'
   },
   tabContainer: {
     marginLeft: 'auto'
@@ -46,22 +57,44 @@ const useStyles = makeStyles(theme => ({
 const Header = props => {
   const classes = useStyles();
 
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, value) => {
+    setValue(value)
+  }
+
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0)
+    } else if(window.location.pathname === '/services' && value !== 1) {
+      setValue(1)
+    } else if(window.location.pathname === '/information' && value !== 2) {
+      setValue(2)
+    } else if(window.location.pathname === '/about-us' && value !== 3) {
+      setValue(3)
+    } else if(window.location.pathname === '/contact-us' && value !== 4) {
+      setValue(4)
+    }
+  }, [value])
+
   return (
     <>
       <ElevationScroll>
         <AppBar position='fixed' color='primary'>
           <Toolbar className={classes.toolbarFlex} disableGutters>
-            <img className={classes.logo} alt='Blob Logo' src={logo} />
+            <Button component={Link} to='/' className={classes.logoContainer} onClick={() => setValue(0)} disableRipple>
+              <img className={classes.logo} alt='Blob Logo' src={logo} />
+            </Button>
             <Typography variant="h6">
-              Blob Development
+              <Link to="/" className={classes.logoLink} onClick={() => setValue(0)}>Blob Development</Link>
             </Typography>
-            <Tabs className={classes.tabContainer}>
-              <Tab className={classes.tab} label="Home" />
-              <Tab className={classes.tab} label="Services" />
-              <Tab className={classes.tab} label="The Information" />
-              <Tab className={classes.tab} label="About Us" />
-              <Tab className={classes.tab} label="Contact Us" />
-            </Tabs>
+            <Tabs value={value} onChange={handleChange} className={classes.tabContainer} indicatorColor='primary'>
+              <Tab className={classes.tab} component={Link} to="/" label="Home" />
+              <Tab className={classes.tab} component={Link} to="/services" label="Services" />
+              <Tab className={classes.tab} component={Link} to="/information" label="The Information" />
+              <Tab className={classes.tab} component={Link} to="/about-us" label="About Us" />
+              <Tab className={classes.tab} component={Link} to="/contact-us" label="Contact Us" />
+            </Tabs> 
             <Button variant="contained" color="secondary" className={classes.button}>
               Free Estimate
             </Button>
