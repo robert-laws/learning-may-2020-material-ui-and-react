78dd9acfae44d17c71c87250f3eac5ab73f835da
Robert Laws <roblaws@me.com>
2020-05-17 12:27:12 +0300

    update logo file for AppBar

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { AppBar, Toolbar, useScrollTrigger, Typography } from '@material-ui/core';
 
+import logo from '../../assets/blob-logo.svg';
+
 function ElevationScroll(props) {
   const { children } = props;
 
@@ -17,7 +19,15 @@ function ElevationScroll(props) {
 
 const useStyles = makeStyles(theme => ({
   ToolbarMargin: {
-    ...theme.mixins.toolbar
+    ...theme.mixins.toolbar,
+    marginBottom: '0.75em'
+  },
+  toolbarFlex: {
+    alignItems: 'flex-start'
+  },
+  logo: {
+    height: '4em',
+    margin: '5px 10px'
   }
 }))
 
@@ -28,8 +38,9 @@ const Header = props => {
     <>
       <ElevationScroll>
         <AppBar position='fixed' color='primary'>
-          <Toolbar>
-            <Typography variant='h6'>
+          <Toolbar className={classes.toolbarFlex} disableGutters>
+            <img className={classes.logo} alt='Blob Logo' src={logo} />
+            <Typography variant="h6">
               Blob Development
             </Typography>
           </Toolbar>
